e02ef4715214effa2baa0b8acf3f6c554141e213
dinlan94 <fajarudin96@gmail.com>
2023-01-30 21:19:10 +0700

    update

---
+++ src/elements/Stepper/index.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+import propTypes from "prop-types";
+
+export default function Stepper(props) {
+    const { steps, initialStep } = props;
+    const stepKeys = Object.keys(steps);
+
+    const [CurrentStep, setCurrentStep] = useState(
+        stepKeys.indexOf(initialStep) > -1 ? initialStep : stepKeys[0]
+    );
+
+    const totalStep = Object.keys(steps).length;
+    const indexStep = stepKeys.indexOf(CurrentStep);
+
+    function prevStep() {
+        if (+indexStep > 0) setCurrentStep(stepKeys[indexStep - 1]);
+    }
+
+    function nexStep() {
+        if (+indexStep < totalStep) setCurrentStep(stepKeys[indexStep + 1]);
+    }
+
+
+    return (
+        <>
+            {props.children(prevStep, nexStep, CurrentStep, steps)}
+        </>
+    );
+}
+
+Stepper.propTypes = {
+    data: propTypes.object.isRequired,
+    initialStep: propTypes.string.isRequired,
+}
+
+export { default as Numbering } from "./Numbering";
+export { default as Meta } from "./Meta";
+export { default as Controller } from "./Controller";
+export { default as MainContent } from "./MainContent";
