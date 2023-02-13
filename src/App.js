aea46b63f186dae7f6021abeca543c3787c228d2
dinlan94 <fajarudin96@gmail.com>
2023-02-13 19:58:17 +0700

    update fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React from "react";
-import { Router, Route, Switch } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import LandingPage from "pages/LandingPage";
-import DetailsPage from "pages/DetailsPage";
-import Checkout from "pages/Checkout";
-import NotFound from "pages/404";
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import LandingPage from 'pages/LandingPage';
+import DetailsPage from 'pages/DetailsPage';
+import Checkout from 'pages/Checkout';
 
 import "assets/scss/style.scss";
 
@@ -12,12 +11,9 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route exact path="/properties/:id" component={DetailsPage} />
-          <Route path="/checkout" component={Checkout} />
-          <Route path="*" component={NotFound} />
-        </Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/properties/:id" component={DetailsPage} />
+        <Route path="/checkout" component={Checkout} />
       </Router>
 
       <ToastContainer></ToastContainer>
